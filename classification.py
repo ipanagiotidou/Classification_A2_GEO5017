ff19d3559488702aa085855bf65a424650e06a98
Ioanna <ioannapanag@yahoo.com>
2022-06-16 18:35:32 +0200

    evaluation methods completed

--- classification.py
+++ classification.py
@@ -22,6 +22,11 @@ def main():
     # Todo: Separate the dataset into training and test set randomly
     x_train,x_test,y_train,y_test=train_test_split(df[["density_3d", "height", "area_3d"]],df['label'],test_size=0.4)
 
+    df = pd.DataFrame(y_test).reset_index()
+    df.columns = ['id', 'label']
+    num_pc = df.groupby(by=['label']).count()
+    num_pc = np.array(num_pc['id'])
+
     # Todo: normalize the data = each attribute value / max possible value of this attribute
     min_max_scaler = preprocessing.MinMaxScaler()
     train_scaled = min_max_scaler.fit_transform(x_train)
@@ -29,19 +34,34 @@ def main():
     test_scaled = min_max_scaler.fit_transform(x_test)
     x_test = pd.DataFrame(test_scaled)
 
-    # Todo: SVM classification
-    clf = svm.SVC()
-    # fit the model to the data
-    clf.fit(np.array(x_train), np.array(y_train))
-    # run the model to independent test data
-    svm_labels_pred = clf.predict(np.array(x_test))
 
-    # Todo: For the SVM classifier, try different Kernel functions and keep the most promising.
+    # Todo: SVM classification, try different kernels and keep the most promising.
+    # todo: call the different kernels at once because every time the sample set changes
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+    labels_SVM_kernels = []
+    for k in kernels:
+        svm_labels_pred = []
+        clf = svm.SVC(kernel='linear') # kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}
+        clf.fit(np.array(x_train), np.array(y_train))
+        svm_labels_pred = clf.predict(np.array(x_test))
+        labels_SVM_kernels.append(svm_labels_pred)
 
 
     # Todo: Evaluation
-    conf_matrix = confusion_matrix(y_test, svm_labels_pred)
-    print(conf_matrix)
+    # Metric: Confusion Matrix
+    for labels_pred in labels_SVM_kernels:
+        conf_matrix = confusion_matrix(y_test, labels_pred) \
+        # Metric: Overall Accuracy
+        overall_accuracy = sum(np.diagonal(conf_matrix)) / len(y_test)
+        print("Overall Accuracy", overall_accuracy)
+        # Metric: Mean per-class accuracy
+        mA = (1/5) * np.sum(np.divide(np.diagonal(conf_matrix), num_pc))
+        print("Mean per class Accuracy: ", mA)
+
+
+
+
+
 
 
     return 0
